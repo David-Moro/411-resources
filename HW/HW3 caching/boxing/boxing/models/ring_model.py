8259924c51964440cb9aeeaddd1c3026a3a43100
khall678 <khall1@bu.edu>
2025-04-11 13:09:41 -0400

    Updated attributes to the ring_model

--- HW/HW3 caching/boxing/boxing/models/ring_model.py
+++ HW/HW3 caching/boxing/boxing/models/ring_model.py
@@ -31,7 +31,10 @@ class RingModel:
             ttl_seconds (int): The time-to-live in seconds for the cached boxer objects.
 
         """
-        pass
+        self.ring: List[int] = []
+        self._boxer_cache: dict[int, Boxers]
+        self.ttl: dict[int, float] = {}
+        self.ttl_seconds = int(os.getenv("TTL", 60))
 
     def fight(self) -> str:
         """Simulates a fight between two combatants.
